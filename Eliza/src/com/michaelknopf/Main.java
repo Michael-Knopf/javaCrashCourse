38d1ac25ea5f3c445dfb88accffb88299ba2fcd1
Michael_Knopf <mikeknopf2@gmail.com>
2021-11-03 19:49:25 -0400

    Eliza program

---
+++ Eliza/src/com/michaelknopf/Main.java
@@ -0,0 +1,23 @@
+package com.michaelknopf;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		var scanner = new Scanner(System.in);
+		
+		System.out.println(Doctor.intro());
+		var userInput = "";
+		
+		while(!userInput.equalsIgnoreCase("quit")){
+			userInput = scanner.nextLine();
+			String response = Doctor.response(userInput);
+			System.out.println(response);
+		}
+		
+		scanner.close();
+	}
+
+}
